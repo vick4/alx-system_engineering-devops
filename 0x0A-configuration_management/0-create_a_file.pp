414003c131e05f8b8665a08fa57cd6d9469ed244
vick4 <victorisaacblw@gmail.com>
2023-11-24 14:52:30 -0800

    done

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,6 +1,7 @@
+# Create a file at /tmp/school with specific attributes
+
 file { '/tmp/school':
   ensure  => file,                  # Ensure it's a regular file
-  path    => '/tmp/school',
   mode    => '0744',                # Set file permissions to 0744
   owner   => 'www-data',            # Set the owner to www-data
   group   => 'www-data',            # Set the group to www-data
