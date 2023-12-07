8e4cf4f7e4aa5fdcca08b30cdb2ef0474ca713ed
Issrock <isaacrockblw@gmail.com>
2023-12-07 11:39:09 -0800

    100-puppet_ssh_config.pp

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+# Automating configuration using Puppet
+
+file { '/ect/ssh/ssh_config':
+	ensure => present,
+
+content  => "
+
+	#SSH client configuration
+	host*
+	IdentityFile ~/.ssh/school
+	PasswordAuthentication no
+
+}
